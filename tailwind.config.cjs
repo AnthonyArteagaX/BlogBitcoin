fb777665b59f2b6d751b847c3ad664c074bd3bce
Anthony <arteaga.anthonyp2021@gmail.com>
2023-05-25 13:50:09 -0500

    Primer version de mi blog

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				boton: '#67e8f9',
+				gris: '#cbd5e1',
+				grisa: '#94a3b8'
+			},
+		},
+	},
+	plugins: [],
+	
+}
